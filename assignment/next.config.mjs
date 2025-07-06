83e9861062e14e0af1f2d202d7bb39e0a84d7971
Pranjul <pranjulsaxena682@gmail.com>
2025-07-06 23:42:15 +0530

    j

--- assignment/next.config.mjs
+++ assignment/next.config.mjs
@@ -1,25 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-  },
-  experimental: {
-    serverComponentsExternalPackages: ["mongodb"],
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Fix for MongoDB build issues
-    if (isServer) {
-      config.externals.push({
-        mongodb: "mongodb",
-      });
-    }
-    return config;
-  },
-  // Ensure environment variables are available during build
-  generateBuildId: async () => {
-    // You can return any string here, will be used as the build id
-    return "build-" + Date.now();
-  },
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
+export default nextConfig;
